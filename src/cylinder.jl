d6107548ab5a11c7539f33c5422eac2bdaffc68c
matthewozon <matthew.ozon@gmail.com>
2023-08-21 09:59:12 +0300

    Distance between a point on a parameteric line and the intersection with a sphere or cylinder
    Co-authored-by: Matthew Ozon matthew.ozon@gmail.com
    Co-authored-by: Konstantin Tumashevich 70195045+Alvaisari@users.noreply.github.com

---
+++ src/cylinder.jl
@@ -0,0 +1,105 @@
+"""
+    determines the distance on the parametric line starting from (xm,ym,zm) in the direction (x0,y0,z0) intersecting with the cylinder centered on the Oy axis and of radius R
+
+    τ_root_cylinder(x0::Cdouble,y0::Cdouble,z0::Cdouble,xm::Cdouble,ym::Cdouble,zm::Cdouble,R2::Cdouble,rm2::Cdouble)
+
+    input: 
+
+      - xm, ym and zm: origin of the parametric line (τ=0)
+      - x0, y0 and z0: coordinates of a point on the parametric line
+      - R2: square radius of the intersecting cylinder 
+      - rm2: square distance between the center of the cylinder and (xm,ym,zm), rm^2=xm^2+ym^2  (R2-rm2⩾0)
+
+    output: 
+
+      - return the distance between the (xm,ym,zm) and the intersection point (cylinder of radius R along the Oy axis and straight line between (xm,ym,zm) and (x0,y0,z0))
+"""
+function τ_root_cylinder(x0::Cdouble,y0::Cdouble,z0::Cdouble,xm::Cdouble,ym::Cdouble,zm::Cdouble,R2::Cdouble,rm2::Cdouble)
+    # angular direction (xm,ym,zm) to (x0,y0,z0)
+    τ_max = sqrt((x0-xm)^2 + (y0-ym)^2 + (z0-zm)^2)
+    α = (x0-xm)/τ_max
+    γ = (z0-zm)/τ_max
+    # discriminant
+    Δ = 4*((zm*γ + xm*α)^2 + (γ^2+α^2)*(R2-rm2))
+    # first root 
+    τ1 = -(zm*γ + xm*α + 0.5sqrt(Δ))/(γ^2+α^2)
+    # second root
+    τ2 = -(zm*γ + xm*α - 0.5sqrt(Δ))/(γ^2+α^2)
+    # projected square distances
+    d1 = ((zm+γ*τ1)-z0)^2 + ((xm+α*τ1)-x0)^2
+    d2 = ((zm+γ*τ2)-z0)^2 + ((xm+α*τ2)-x0)^2
+    # select the root such that the distance to the apperture is shortest
+    if (d1<d2)
+        τ_bar = τ1
+    else
+        τ_bar = τ2
+    end
+
+    τ_bar,α,γ
+end
+
+
+"""
+    quadrature evaluating the integral ∫_{τmin}^{τmax} f(g(τ)) dτ for a cylinder of radius μ0 (along the axis Oy)
+
+
+    with g(τ)=√(a*τ^2+b*τ+c) with a, b and c defined for each position in the cylinder (depend on the location in the cylinder and the direction of the parametric line)
+
+    f(r) is a mass function depending depending on the distance from the center of the cylinder 
+
+    the integral boundaries are such that the parametric curve satifies τmin->(r[n],θ[j],y[k]) and in τmax->(r(τmax),θ(τmax),y(τmax)) with r(τmax)=μ0+κΔr 
+    τmin is set to 0 without loss of generality and τmax is determined for each position in the cylinder 
+
+
+    quadrature_fg_cylinder(r::Array{Cdouble,1},θ::Array{Cdouble,1},y::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+
+    input:
+
+      - r,θ,y: cylindrical coordinates of the discretization of the cylinder (symmetry axis Oy)
+      - x0,y0,z0: cartesian coordinate of a point belonging to the parametric straight line of integration
+      - μ0: radius of the cylinder
+      - Δr: transition distance of the mass function f
+    
+    optional input:
+
+      - κ:  maximum radius for the integration is r=μ0+κΔr
+      - Nτ: number of discretization nodes 
+
+    output:
+
+      - FGQ_rθy: 3D array containing the quadrature for each discretization nodes (r,θ,y)
+      
+"""
+function quadrature_fg_cylinder(r::Array{Cdouble,1},θ::Array{Cdouble,1},y::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+    # how far out of the sample should we discretize
+    R2 = (μ0 + κ*Δr)^2
+
+    # compute the gain
+    FGQ_rθy = zeros(Cdouble,length(r),length(θ),length(y));
+    for n in eachindex(r)
+        rm2 = r[n]^2
+        if (R2>=rm2)
+            for j in eachindex(θ)
+                # cartesian coordinates
+                xm  = r[n]*sin(θ[j])
+                zm  = r[n]*cos(θ[j])
+                for k in eachindex(y)
+                    # quadrature ∫_0^{τ_root} f(g(τ)) dτ
+                    τ_root,α,γ = τ_root_cylinder(x0,y0,z0,xm,y[k],zm,R2,rm2)
+                    # this is not optimal, the details are at the transition, so, the discretization of τ should reflect that
+                    τ_disc = collect(LinRange(0.0,τ_root,Nτ)) 
+                    r_param = sqrt.((xm.+α*τ_disc).^2 .+ (zm.+γ*τ_disc).^2)
+                    # a = α^2 + γ^2
+                    # b = 2.0*(α*xm + γ*zm)
+                    # c = xm^2 + zm^2;
+                    # r_param = g_sqrtquad.(τ_disc,a,b,c)
+                    ρ_disc = f_logistic(r_param,μ0,Δr;A=1.0);
+                    FGQ_rθy[n,j,k] = quadrature_fg(ρ_disc,τ_disc)
+                end
+            end
+        end
+    end
+
+    # return 
+    FGQ_rθy
+end
---
+++ src/sphere.jl
@@ -0,0 +1,108 @@
+"""
+    determines the distance on the parametric line starting from (xm,ym,zm) in the direction (x0,y0,z0)
+
+    τ_root_sphere(x0::Cdouble,y0::Cdouble,z0::Cdouble,xm::Cdouble,ym::Cdouble,zm::Cdouble,R2::Cdouble,rm2::Cdouble)
+
+    input: 
+
+      - xm, ym and zm: origin of the parametric line (τ=0)
+      - x0, y0 and z0: coordinates of a point on the parametric line
+      - R2: square radius of the intersecting sphere 
+      - rm2: square distance between the center of the sphere and (xm,ym,zm), rm^2=xm^2+ym^2+zm^2  (R2-rm2⩾0)
+
+    output: 
+
+      - return the distance between the (xm,ym,zm) and the intersection point (sphere of radius R and straight line between (xm,ym,zm) and (x0,y0,z0))
+"""
+function τ_root_sphere(x0::Cdouble,y0::Cdouble,z0::Cdouble,xm::Cdouble,ym::Cdouble,zm::Cdouble,R2::Cdouble,rm2::Cdouble)
+    # angular direction (xm,ym,zm) to (x0,y0,z0)
+    τ_max = sqrt((x0-xm)^2 + (y0-ym)^2 + (z0-zm)^2)
+    α = (x0-xm)/τ_max
+    β = (y0-ym)/τ_max
+    γ = (z0-zm)/τ_max
+    # discriminant
+    Δ = 4*((xm*α + ym*β+ zm*γ)^2 + (R2-rm2))
+    # first root 
+    τ1 = -(xm*α + ym*β + zm*γ + 0.5sqrt(Δ))
+    # second root
+    τ2 = -(xm*α + ym*β + zm*γ - 0.5sqrt(Δ))
+    # projected square distances
+    d1 = ((xm+α*τ1)-x0)^2 + ((ym+β*τ1)-y0)^2 + ((zm+γ*τ1)-z0)^2
+    d2 = ((xm+α*τ2)-x0)^2 + ((ym+β*τ2)-y0)^2 + ((zm+γ*τ2)-z0)^2
+    # select the root such that the distance to the point (x0,y0,z0) is shortest
+    if (d1<d2)
+        τ_bar = τ1
+    else
+        τ_bar = τ2
+    end
+
+    # return the distance and the direction cos 
+    τ_bar,α,β,γ
+end
+
+
+"""
+    quadrature evaluating the integral ∫_{τmin}^{τmax} f(g(τ)) dτ for a sphere of radius μ0
+
+    with g(τ)=√(a*τ^2+b*τ+c) with a, b and c defined for each position in the sphere (depend on the location in the sphere and the direction of the parametric line)
+
+    f(r) is a mass function depending depending on the distance from the center of the sphere 
+
+    the integral boundaries are such that the parametric curve satifies τmin->(r[n],φ[j],θ[k]) and in τmax->(r(τmax),φ(τmax),θ(τmax)) with r(τmax)=μ0+κΔr 
+    τmin is set to 0 without loss of generality and τmax is determined for each position in the sphere 
+
+    quadrature_fg_sphere(r::Array{Cdouble,1},φ::Array{Cdouble,1},θ::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+
+    input:
+
+      - r,φ,θ: spherical coordinates of the discretization of the sphere (polar axis Oz)
+      - x0,y0,z0: cartesian coordinate of a point belonging to the parametric straight line of integration
+      - μ0: radius of the sphere 
+      - Δr: transition distance of the mass function f
+    
+    optional input:
+
+      - κ:  maximum radius for the integration is r=μ0+κΔr
+      - Nτ: number of discretization nodes 
+
+    output:
+
+      - FGQ_rφθ: 3D array containing the quadrature for each discretization nodes (r,φ,θ)
+"""
+function quadrature_fg_sphere(r::Array{Cdouble,1},φ::Array{Cdouble,1},θ::Array{Cdouble,1},x0::Cdouble,y0::Cdouble,z0::Cdouble,μ0::Cdouble,Δr::Cdouble;κ::Cdouble=5.0,Nτ::Int64=20)
+    # how far out of the sample should we discretize
+    R2 = (μ0 + κ*Δr)^2
+
+    # compute quadrature for each discretization nodes (r,φ,θ)
+    FGQ_rφθ = zeros(Cdouble,length(r),length(φ),length(θ));
+    for n in eachindex(r)
+        rm2 = r[n]^2
+        if (R2>=rm2)
+            for j in eachindex(φ)
+                zm   = r[n]*cos(φ[j])
+                rxym = r[n]*sin(φ[j])
+                for k in eachindex(θ)
+                    # cartesian coordinates
+                    xm = rxym*cos(θ[k])
+                    ym = rxym*sin(θ[k])
+
+                    # quadrature ∫_0^{τ_root} f(g(τ)) dτ
+                    τ_root,α,β,γ = τ_root_sphere(x0,y0,z0,xm,ym,zm,R2,rm2)
+                    # this is not optimal, the details are at the transition, so, the discretization of τ should reflect that
+                    τ_disc = collect(LinRange(0.0,τ_root,Nτ)) 
+                    r_param = sqrt.((xm.+α*τ_disc).^2 .+ (ym.+β*τ_disc).^2 .+ (zm.+γ*τ_disc).^2)
+                    # a = α^2 + β^2 + γ^2
+                    # b = 2.0*(α*xm + β*ym + γ*zm)
+                    # c = xm^2 + ym^2 + zm^2;
+                    # r_param = g_sqrtquad.(τ_disc,a,b,c)
+                    ρ_disc = f_logistic(r_param,μ0,Δr;A=1.0);
+                    # quadrature f(g(τ))
+                    FGQ_rφθ[n,j,k] = quadrature_fg(ρ_disc,τ_disc)
+                end
+            end
+        end
+    end
+
+    # return the quadrature for each discretization nodes of the sphere 
+    FGQ_rφθ
+end
